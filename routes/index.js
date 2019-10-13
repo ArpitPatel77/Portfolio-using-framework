3e6d42339e76b4013a4c9ebbf734e141ba374c90
Arpit <200393479@student.georgianc.on.ca>
2019-10-13 13:14:10 -0400

    Added about page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+ res.render('about', { title: 'Express' });
+});
+
+router.get('/project', function(req, res, next) {
+ res.render('project', { title: 'Express' });
+});
+
+router.get('/services', function(req, res, next) {
+ res.render('services', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+ res.render('contact', { title: 'Express' });
+});
+
 module.exports = router;
